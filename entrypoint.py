2817a1ff720c2877f2926621009e956fcc4bafd7
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 20:19:23 -0400

    Mount PROCURE KPI publisher

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,6 @@
 from app import app
 from inbound_layers import router as inbound_router
+from procure_kpis import router as procure_kpis_router
 
 app.include_router(inbound_router)
+app.include_router(procure_kpis_router)
